f538ea8c9671d850c14b77adefcbe1df1fa6d4f1
Nishantwedone <nishantkumargupta728@gmail.com>
2024-09-07 01:20:26 +0530

    some changes in front

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Login from './Login'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Login from './Login';
 import Browse from './Browse'
 
 const Body = () => {
