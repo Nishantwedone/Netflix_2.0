bf68d5854505a3c42dec132f1a7387fb3c6181d1
Nishant Gupta <nishantgupta@Nishants-MacBook-Air.local>
2024-09-06 22:46:09 +0530

    now stop the fontend and lets start backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,6 +11,13 @@ const Login = () => {
   const loginHandler = () =>{
     setIsLogin(!isLogin);
   }
+  const getInputData = (e)=>{
+    e.preventDefault();
+    console.log(fullName,email,password);
+    setFullName("");
+    setEmail("");
+    setPassword("");  
+  }
 
   return (
     <div>
@@ -19,7 +26,7 @@ const Login = () => {
         <img className='w-[100vw] h-[100vh] bg-cover' src='https://wallpapers.com/images/hd/netflix-background-gs7hjuwvv2g0e9fj.jpg' alt='error' />
       </div>
 
-      <form className='absolute flex flex-col left-0 right-0 mx-auto w-3/12 my-36 items-center justify-center bg-black p-12 opacity-90 rounded-md'>
+      <form onSubmit={getInputData} className='absolute flex flex-col left-0 right-0 mx-auto w-3/12 my-36 items-center justify-center bg-black p-12 opacity-90 rounded-md'>
         <div className='flex flex-col items-center'>
           <h1 className='text-white text-3xl mb-4 font-bold'>{isLogin? "Login" : "Signup"}</h1>
           {
@@ -28,6 +35,7 @@ const Login = () => {
          
           <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' type='email' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white'></input>
           <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' type='password' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white'></input>
+          <button className='text-white bg-red-600 mt-4 p-3 px-[6.4vw] rounded-sm font-bold'>{isLogin? "Login" : "Signup"}</button>
           <p className='text-white mt-2 tracking-tight'>{isLogin? "New to Netflix?" : "Already have an account"}<span onClick={loginHandler} className='ml-2 text-blue-900  font-medium cursor-pointer'>{isLogin? "Signup" : "Login"}</span></p>
         </div>
       </form>
